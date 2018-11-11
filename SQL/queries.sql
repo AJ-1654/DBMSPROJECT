0f402b47b4d8c7ea7da61bb98b9999b012c2a031
AJ-1654 <anish.jangra11@hotmail.com>
2018-11-11 16:34:16 +0530

    sql queries added

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -1,8 +1,20 @@
--- to get tasks for a student whose teacher_id will be procured from request.body
-SELECT * FROM `tasks` WHERE teacher_id = 'param';
+-- to get the student with particular id; the id will be provided in request body
+SELECT * FROM students WHERE student_id = 'param'; 
 
--- to get all the students under the teacher; the teacher_id will be procured from request.body
-SELECT * FROM `students` WHERE teacher_id = 'param';
+-- to get the teacher with particular id; the id will be provided in request body
+SELECT * FROM teachers WHERE teacher_id = 'param';
 
--- to get all the tasks given by the teacher; the teacher_id will be procured from request.body
-SELECT * FROM `tasks` WHERE teacher_id = 'param';
+-- to get the tasks given by a teacher; the teacher_id will be provided in the request body
+SELECT * FROM tasks WHERE teacher_id = 'param';
+
+-- to insert a new task in the tasks table by a teacher; the task will be provided in the request body
+INSERT INTO tasks SET 'task';
+
+--to get the task from tasks to be updated; task_id will be provided in request parameter
+SELECT * FROM tasks WHERE task_id = 'param';
+
+-- to update a task by the teacher; task_id and updated task will be provided in request parameter and body respectively
+update tasks SET task WHERE task_id = 'param';
+
+-- to delete a task by the teacher; task_id will be provided in request parameters
+DELETE FROM tasks WHERE task_id = 'param';
